584578b731431b0983d7770614a62e24ac38f0fa
Evan Chiu <evan@evanchiu.com>
2013-10-24 18:56:38 -0400

    Created sample index with style

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Evan Chiu">
+    <link rel="shortcut icon" href="/images/favicon.png">
+
+    <title>Tele</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="css/tele.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Tele</h1>
+        <p>These television recordings are available</p>
+      </div>
+    </div>
+
+    <div class="container" id="show-container">
+
+      <div class="col-lg-4 col-sm-6 show-box">
+        <h2>Alpha</h2>
+        <p>Date</p>
+      </div>
+      <div class="col-lg-4 col-sm-6 show-box">
+        <h2>Bravo</h2>
+        <p>Date</p>
+      </div>
+      <div class="col-lg-4 col-sm-6 show-box">
+        <h2>Charlie</h2>
+        <p>Date</p>
+      </div>
+      <div class="col-lg-4 col-sm-6 show-box">
+        <h2>Delta</h2>
+        <p>Date</p>
+      </div>
+      <div class="col-lg-4 col-sm-6 show-box">
+        <h2>Echo</h2>
+        <p>Date</p>
+      </div>
+      <div class="col-lg-4 col-sm-6 show-box">
+        <h2>Foxtrot</h2>
+        <p>Date</p>
+      </div>
+
+      <hr />
+
+      <footer>
+        <p>&copy;2013 <a href = "http://evanchiu.com">Evan Chiu</a></p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/tele.js"></script>
+  </body>
+</html>
