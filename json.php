c0d122f5c715267b3ef0d767d3f850515cab1d67
Evan Chiu <evan@evanchiu.com>
2014-12-04 10:30:28 -0500

    Adding error handling/reporting

--- json.php
+++ json.php
@@ -6,35 +6,43 @@
 date_default_timezone_set('America/New_York');
 
 // Use curl to get listings from tele
-$ch = curl_init("http://10.0.0.11:1077");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$data = curl_exec($ch);
-
-$links = getLinks($data);
 $shows = array();
-foreach ($links as $link) {
-    if (endsWith($link, 'wtv')) {
-        // parse out show title and date
-        preg_match('/\/(.*)_(\w+)_(\d+)_(\d+)_(\d+)_(\d+)_(\d+)_(\d+).wtv/', $link, $matches);
-        $date = new DateTime($matches[3] . '-' . $matches[4] . '-' . $matches[5]
-            . ' ' . $matches[6] . ':' . $matches[7] . ':' . $matches[8]);
-        $title = $matches[1];
-        if (!array_key_exists($title, $shows)) {
-            // Add show to array
-            $shows[$title] = array(array(
-                'date' => $date->format('l, M j'),
-                'timestamp' => $date->getTimeStamp()
-            ));
-        } else {
-            // Add this date instance
-            $shows[$title][] = array(
-                'date' => $date->format('l, M j'),
-                'timestamp' => $date->getTimeStamp()
-            );
+$json = array();
+
+try {
+    $ch = curl_init("http://10.0.0.11:1077");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+
+    $links = getLinks($data);
+    foreach ($links as $link) {
+        if (endsWith($link, 'wtv')) {
+            // parse out show title and date
+            preg_match('/\/(.*)_(\w+)_(\d+)_(\d+)_(\d+)_(\d+)_(\d+)_(\d+).wtv/', $link, $matches);
+            $date = new DateTime($matches[3] . '-' . $matches[4] . '-' . $matches[5]
+                . ' ' . $matches[6] . ':' . $matches[7] . ':' . $matches[8]);
+            $title = $matches[1];
+            if (!array_key_exists($title, $shows)) {
+                // Add show to array
+                $shows[$title] = array(array(
+                    'date' => $date->format('l, M j'),
+                    'timestamp' => $date->getTimeStamp()
+                ));
+            } else {
+                // Add this date instance
+                $shows[$title][] = array(
+                    'date' => $date->format('l, M j'),
+                    'timestamp' => $date->getTimeStamp()
+                );
+            }
         }
     }
+} catch (Exception $e) {
+    $json['error'] = $e->getMessage();
 }
-print json_encode(array('shows' => $shows));
+
+$json['shows'] = $shows;
+print json_encode($json);
 
 # Source from Stack Overflow:
 # http://stackoverflow.com/questions/834303/php-startswith-and-endswith-functions
